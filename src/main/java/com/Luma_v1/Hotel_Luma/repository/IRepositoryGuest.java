642edc3191a6f99438f7924dabea3c4798c4c2f2
Takitouh <1097097476jhon@gmail.com>
2025-08-25 11:10:57 -0500

    add query method to find all the bookings by guest email

--- src/main/java/com/Luma_v1/Hotel_Luma/repository/IRepositoryGuest.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/repository/IRepositoryGuest.java
@@ -1,9 +1,15 @@
 package com.Luma_v1.Hotel_Luma.repository;
 
+import com.Luma_v1.Hotel_Luma.dto.ResponseRoomNumAndBookingDateDTO;
 import com.Luma_v1.Hotel_Luma.entity.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IRepositoryGuest extends JpaRepository<Guest, Long> {
+    @Query(value = "SELECT CONCAT(g.first_name, ' ', g.last_name) AS customer,r.number AS roomNumber, b.check_in AS checkIn, b.check_out AS checkOut FROM guest g INNER JOIN booking b ON g.id = b.guest_id INNER JOIN room r ON r.id = b.room_id WHERE  g.email LIKE :email", nativeQuery = true)
+    List<ResponseRoomNumAndBookingDateDTO> findBookingsByGuestEmail(String email);
 }
