1b99cebfe619720b7ecbecfdd8876880af63325d
Takitouh <1097097476jhon@gmail.com>
2025-08-25 11:10:00 -0500

    add custom exceptions for booking validation

---
+++ src/main/java/com/Luma_v1/Hotel_Luma/exceptionHandler/exceptions/BookingCheckInOrCheckOutInvalidException.java
@@ -0,0 +1,7 @@
+package com.Luma_v1.Hotel_Luma.exceptionHandler.exceptions;
+
+public class BookingCheckInOrCheckOutInvalidException extends RuntimeException {
+    public BookingCheckInOrCheckOutInvalidException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/Luma_v1/Hotel_Luma/exceptionHandler/exceptions/BookingDateCrossInvalidException.java
@@ -0,0 +1,7 @@
+package com.Luma_v1.Hotel_Luma.exceptionHandler.exceptions;
+
+public class BookingDateCrossInvalidException extends RuntimeException {
+    public BookingDateCrossInvalidException(String message) {
+        super(message);
+    }
+}
