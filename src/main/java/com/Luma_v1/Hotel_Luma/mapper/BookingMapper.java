cd1a4b2c73a1b884c26ab8032761a8f8fea867b0
Takitouh <1097097476jhon@gmail.com>
2025-08-27 10:50:17 -0500

    add booking type mapping to ResponseBookingDTO in BookingMapper

--- src/main/java/com/Luma_v1/Hotel_Luma/mapper/BookingMapper.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/mapper/BookingMapper.java
@@ -22,11 +22,13 @@ public abstract class BookingMapper {
 
     @Mapping(source = "guest", target = "guestName")
     @Mapping(source = "room", target = "roomNumber")
+    @Mapping(source = "type", target = "type")
     public abstract ResponseBookingDTO toResponseDTO(Booking booking);
 
     @Mapping(source = "checkIn", target = "checkIn")
     @Mapping(source = "checkOut", target = "checkOut")
     @Mapping(source = "status", target = "status")
+    @Mapping(source = "type", target = "type")
     @Mapping(source = "totalPrice", target = "totalPrice")
     @Mapping(source = "guestId", target = "guest")
     @Mapping(source = "roomId", target = "room")
@@ -36,6 +38,7 @@ public abstract class BookingMapper {
     @Mapping(source = "checkIn", target = "checkIn")
     @Mapping(source = "checkOut", target = "checkOut")
     @Mapping(source = "status", target = "status")
+    @Mapping(source = "type", target = "type")
     @Mapping(source = "totalPrice", target = "totalPrice")
     @Mapping(source = "guestId", target = "guest")
     @Mapping(source = "roomId", target = "room")
@@ -45,6 +48,7 @@ public abstract class BookingMapper {
     @Mapping(source = "checkIn", target = "checkIn")
     @Mapping(source = "checkOut", target = "checkOut")
     @Mapping(source = "status", target = "status")
+    @Mapping(source = "type", target = "type")
     @Mapping(source = "guestId", target = "guest")
     @Mapping(source = "roomId", target = "room")
     @Mapping(source = "totalPrice", target = "totalPrice")
