8af66f6558fe9e6d8b0483a50e2eba1078f13ed2
Takitouh <1097097476jhon@gmail.com>
2025-08-27 10:49:38 -0500

    add schedule times for check-in, check-out, and day use to hotel DTOs

--- src/main/java/com/Luma_v1/Hotel_Luma/dto/CreateHotelDTO.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/dto/CreateHotelDTO.java
@@ -1,13 +1,22 @@
 package com.Luma_v1.Hotel_Luma.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-import java.util.List;
+import java.time.LocalTime;
 
 public record CreateHotelDTO(
         @NotBlank(message = "Name can't be null or blank")
         String name,
         @NotBlank(message = "Location can't be null or blank")
-        String location
+        String location,
+        @NotNull(message = "Schedule check-in can't be null")
+        LocalTime scheduleCheckIn,
+        @NotNull(message = "Schedule check-out can't be null")
+        LocalTime scheduleCheckOut,
+        @NotNull(message = "Schedule day use start can't be null")
+        LocalTime scheduleDayUseStart,
+        @NotNull(message = "Schedule day use end can't be null")
+        LocalTime scheduleDayUseEnd
 ) {
 }
--- src/main/java/com/Luma_v1/Hotel_Luma/dto/PatchHotelDTO.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/dto/PatchHotelDTO.java
@@ -1,10 +1,16 @@
 package com.Luma_v1.Hotel_Luma.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public record PatchHotelDTO(
         String name,
         String location,
+        LocalTime scheduleCheckIn,
+        LocalTime scheduleCheckOut,
+        LocalTime scheduleDayUseStart,
+        LocalTime scheduleDayUseEnd,
         List<CreateRoomDTO> rooms
 ) {
 }
