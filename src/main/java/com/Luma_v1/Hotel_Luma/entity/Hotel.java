d24d16ad7ba2f789ae425392d970f4a9f011e9cd
Takitouh <1097097476jhon@gmail.com>
2025-08-27 10:48:48 -0500

    add scheduled check-in and check-out times to Hotel entity

--- src/main/java/com/Luma_v1/Hotel_Luma/entity/Hotel.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/entity/Hotel.java
@@ -3,6 +3,8 @@ package com.Luma_v1.Hotel_Luma.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +12,20 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class Hotel {
-    @Id 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
     private String location;
-    
+
+    private LocalTime scheduleCheckIn; // e.g., 14:00
+    private LocalTime scheduleCheckOut; // e.g., 10:00
+
+    private LocalTime scheduleDayUseStart; // e.g., 8:00
+    private LocalTime scheduleDayUseEnd; // e.g., 20:00
+
     @OneToMany(mappedBy = "hotel")
     private List<Room> rooms = new ArrayList<>();
 }
