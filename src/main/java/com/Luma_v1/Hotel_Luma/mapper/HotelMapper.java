41d993b09cd515800fc8924c63ef07f3bb29122e
Takitouh <1097097476jhon@gmail.com>
2025-08-27 10:50:30 -0500

    add schedule times to HotelMapper for check-in, check-out, and day use

--- src/main/java/com/Luma_v1/Hotel_Luma/mapper/HotelMapper.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/mapper/HotelMapper.java
@@ -10,23 +10,43 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring", uses = {RoomMapper.class})
 public interface HotelMapper {
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "location", target = "location")
+    @Mapping(source = "scheduleCheckIn", target = "scheduleCheckIn")
+    @Mapping(source = "scheduleCheckOut", target = "scheduleCheckOut")
+    @Mapping(source = "scheduleDayUseStart", target = "scheduleDayUseStart")
+    @Mapping(source = "scheduleDayUseEnd", target = "scheduleDayUseEnd")
+    @Mapping(source = "rooms", target = "rooms")
     ResponseHotelDTO toResponseDTO(Hotel hotel);
 
     @Mapping(source = "location", target = "location")
     @Mapping(source = "name", target = "name")
-//    @Mapping(target = "rooms", ignore = true)
+    @Mapping(source = "scheduleCheckIn", target = "scheduleCheckIn")
+    @Mapping(source = "scheduleCheckOut", target = "scheduleCheckOut")
+    @Mapping(source = "scheduleDayUseStart", target = "scheduleDayUseStart")
+    @Mapping(source = "scheduleDayUseEnd", target = "scheduleDayUseEnd")
+    @Mapping(target = "rooms", ignore = true)
     @Mapping(target = "id", ignore = true)
     Hotel toEntity(CreateHotelDTO createHotelDTO);
 
     @Mapping(source = "location", target = "location")
     @Mapping(source = "name", target = "name")
-//    @Mapping(target = "rooms", ignore = true)
+    @Mapping(source = "scheduleCheckIn", target = "scheduleCheckIn")
+    @Mapping(source = "scheduleCheckOut", target = "scheduleCheckOut")
+    @Mapping(source = "scheduleDayUseStart", target = "scheduleDayUseStart")
+    @Mapping(source = "scheduleDayUseEnd", target = "scheduleDayUseEnd")
+    @Mapping(target = "rooms", ignore = true)
     @Mapping(target = "id", ignore = true)
     Hotel toEntity(PutHotelDTO putHotelDTO);
 
     @Mapping(source = "location", target = "location")
     @Mapping(source = "name", target = "name")
-//    @Mapping(target = "rooms", ignore = true)
+    @Mapping(source = "scheduleCheckIn", target = "scheduleCheckIn")
+    @Mapping(source = "scheduleCheckOut", target = "scheduleCheckOut")
+    @Mapping(source = "scheduleDayUseStart", target = "scheduleDayUseStart")
+    @Mapping(source = "scheduleDayUseEnd", target = "scheduleDayUseEnd")
+    @Mapping(source = "rooms", target = "rooms")
     @Mapping(target = "id", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Hotel toEntity(PatchHotelDTO patchHotelDTO, @MappingTarget Hotel hotel);
