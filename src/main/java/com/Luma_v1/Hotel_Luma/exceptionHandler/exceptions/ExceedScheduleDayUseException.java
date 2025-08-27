2b0d6518ed0d55aed60fb71e0d4e17800cb00443
Takitouh <1097097476jhon@gmail.com>
2025-08-27 10:45:57 -0500

    add custom exception for exceeding scheduled day use

---
+++ src/main/java/com/Luma_v1/Hotel_Luma/exceptionHandler/exceptions/ExceedScheduleDayUseException.java
@@ -0,0 +1,7 @@
+package com.Luma_v1.Hotel_Luma.exceptionHandler.exceptions;
+
+public class ExceedScheduleDayUseException extends RuntimeException {
+    public ExceedScheduleDayUseException(String message) {
+        super(message);
+    }
+}
