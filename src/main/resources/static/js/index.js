2ebd57096895e9b6ea66ece8101b3b07b418ebac
Takitouh <1097097476jhon@gmail.com>
2025-09-20 08:39:52 -0500

    feat: implement search functionality with redirect and display message for no results

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,16 +1,43 @@
-//Fetch to the REST API
+//Function to get the value of the search input and after redirect to the result
 
+function redirectAfterSearch(){
+    const searchInp = document.getElementById("searchInput");
+    // const url = "localhost:8080/api/v1/hotels/find-by-name?name=" + searchInp.value;
+
+    var params = new URLSearchParams();
+    params.append('name', searchInp.value);
+
+    const url = "searchResults.html?" + params.toString();
+
+    location.href = url;
+
+    // console.log(url)
+    // document.location.href = "http://localhost:8080/api/v1/hotels/find-by-name?name=" + searchInp.value;
+    // return url;
+}
+
+//Create the button element
+const searchButton = document.getElementById("search-bar-button");
+//Add the event listener to the button
+searchButton.addEventListener("click", redirectAfterSearch)
 
 fetch('/api/v1/hotels/get')
     .then(res => {
         if (!res.ok) throw new Error('Error in the server reply.');
         return res.json();
     })
-    .then(hotels => {
+    .then(async hotels => {
         console.log('Hotels received:', hotels);
 
         const container = document.getElementById("hotels");
         container.innerHTML = "";
+
+        if (await hotels == null){
+            const title = document.createElement("h2");
+            title.textContent = "No hotels found.";
+            container.appendChild(title);
+            return;
+        }
 
         hotels.forEach(hotel => {
             console.log(hotel.name, hotel.location);
