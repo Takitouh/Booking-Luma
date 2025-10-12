1732db4435f27f18210aba35f33f5d582507f24f
Takitouh <1097097476jhon@gmail.com>
2025-10-11 20:50:15 -0500

    refactor: fixed @Query that managed the disponibility of rooms

--- src/main/java/com/Luma_v1/Hotel_Luma/repository/IRepositoryBooking.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/repository/IRepositoryBooking.java
@@ -9,8 +9,9 @@ import java.time.LocalDate;
 
 @Repository
 public interface IRepositoryBooking extends JpaRepository<Booking, Long> {
-    //Check if the booking date is valid or if there is dates crossing
-    @Query(value = "SELECT COUNT(b.id) FROM booking b WHERE b.room_id = :RoomID AND (:checkInDate BETWEEN b.check_in AND b.check_out OR :checkOutDate BETWEEN b.check_in AND b.check_out) AND b.status <> 'CANCELLED'"
+    //Check if the booking date is valid or if there are dates crossing
+    @Query(value = "SELECT COUNT(b.id) FROM booking b WHERE b.room_id = :RoomID AND (:checkInDate BETWEEN b.check_in AND b.check_out OR :checkOutDate BETWEEN b.check_in AND b.check_out) OR (b.check_in BETWEEN :checkInDate AND :checkOutDate OR b.check_out BETWEEN :checkInDate AND :checkOutDate) AND b.status <> 'CANCELLED'"
             , nativeQuery = true)
+
     Integer givenCheckInAndCheckOutAndRoomID_CheckValidBookingDate(Long RoomID, LocalDate checkInDate, LocalDate checkOutDate);
 }
