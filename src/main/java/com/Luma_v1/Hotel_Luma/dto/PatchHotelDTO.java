01d00b46130357d5a75623ab615234f6dc69eec3
Takitouh <1097097476jhon@gmail.com>
2025-10-30 18:19:15 -0500

    feat: add description field to PatchHotelDTO and PutHotelDTO with validation

--- src/main/java/com/Luma_v1/Hotel_Luma/dto/PatchHotelDTO.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/dto/PatchHotelDTO.java
@@ -1,6 +1,5 @@
 package com.Luma_v1.Hotel_Luma.dto;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
@@ -8,6 +7,7 @@ import java.util.Set;
 public record PatchHotelDTO(
         String name,
         String location,
+        String description,
         LocalTime scheduleCheckIn,
         LocalTime scheduleCheckOut,
         List<CreateRoomDTO> rooms,
--- src/main/java/com/Luma_v1/Hotel_Luma/dto/PutHotelDTO.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/dto/PutHotelDTO.java
@@ -3,13 +3,14 @@ package com.Luma_v1.Hotel_Luma.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
 
 public record PutHotelDTO(
         @NotBlank(message = "Name can't be null or blank")
         String name,
+        @NotBlank(message = "Description can't be null or blank")
+        String description,
         @NotBlank(message = "Location can't be null or blank")
         String location,
         @NotNull(message = "Schedule check-in can't be null")
