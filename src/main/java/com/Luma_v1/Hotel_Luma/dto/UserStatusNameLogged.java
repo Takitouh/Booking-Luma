620f70bc4eec6042730f5b0123352058ef5ce1ac
Takitouh <1097097476jhon@gmail.com>
2025-11-09 14:58:03 -0500

    feat: expand UserStatusNameLogged record to include id and email fields

--- src/main/java/com/Luma_v1/Hotel_Luma/dto/UserStatusNameLogged.java
+++ src/main/java/com/Luma_v1/Hotel_Luma/dto/UserStatusNameLogged.java
@@ -1,4 +1,4 @@
 package com.Luma_v1.Hotel_Luma.dto;
 
-public record UserStatusNameLogged(String fullName, boolean isLogged) {
+public record UserStatusNameLogged(Long id, String fullName, String email, boolean isLogged) {
 }
